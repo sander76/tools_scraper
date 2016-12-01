68adfe9b650e97f29052bb979d292c1c0c0ee224
sanders76 <s.teunissen@gmail.com>
2016-12-01 12:13:21 +0100

    adding json config

--- single_file.py
+++ single_file.py
@@ -2,19 +2,19 @@ import argparse
 import json
 import logging
 from pdf_scraper import Scraper
-
+lgr=logging.getLogger(__name__)
 parser = argparse.ArgumentParser()
 parser.add_argument("jsfile", help="the site to scrape")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     args = parser.parse_args()
-    config_file = args.json
+    config_file = args.jsfile
     with open(config_file) as fl:
         _js=json.load(fl)
-    logging.DEBUG(_js['site'])
-    logging.DEBUG(_js['pdf_scraper'])
-    logging.DEBUG(_js['output'])
+    lgr.debug(_js['site'])
+    lgr.debug(_js['pdf_scraper'])
+    lgr.debug(_js['output'])
 
     pdfer = Scraper('', _js['pdf_scraper'], _js['output'])
     pdfer.create_pdf(_js['site'], _js['output'] + '/test.pdf')
